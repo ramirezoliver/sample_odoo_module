50dacd7ca88e1820b69101e36f21bc56957c5c82
Oliver Ramirez <ramirezoliver.g@gmail.com>
2022-04-15 21:42:09 +0800

    add minimal module __manifest__

---
+++ oli_new_module/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "A Module",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Author Name",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+    ],
+    'css': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
